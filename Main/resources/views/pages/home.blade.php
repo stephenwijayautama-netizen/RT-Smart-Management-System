404eec75c74c961a08122ff8ff9d4b7b2fe89908
stephenwijayautama-netizen <stephenwijayautama@gmail.com>
2026-03-14 17:34:52 +0700

    update for envirotment Api Vercel

--- Main/resources/views/pages/home.blade.php
+++ Main/resources/views/pages/home.blade.php
@@ -1,48 +1,41 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=
-    , initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-     @vite(['resources/css/app.css'])
-    <title>Home</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel</title>
+
+    <style>
+        body {
+            margin: 0;
+            font-family: Arial, Helvetica, sans-serif;
+            background-color: #f3f4f6;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+        }
+
+        .container {
+            text-align: center;
+        }
+
+        h1 {
+            font-size: 48px;
+            margin-bottom: 10px;
+            color: #ef4444;
+        }
+
+        p {
+            font-size: 18px;
+            color: #6b7280;
+        }
+    </style>
 </head>
-
-<body class="bg-[#DCE3EE]">
-     <aside 
-        class="fixed top-0 left-0 w-64 ml-5  mt-5 bg-white shadow-2xl transition-transform duration-300 z-40 md:translate-x-0 md:relative md:block rounded-3xl">
-
-        <div class="p-6">
-            <h2 class="text-2xl font-bold text-blue-600 mb-6">
-                My Dashboard
-            </h2>
-
-            <ul class="space-y-4">
-                <li>
-                    <a href="#" class="block p-3 rounded-xl hover:bg-blue-100">
-                        Dashboard
-                    </a>
-                </li>
-                <li>
-                    <a href="#" class="block p-3 rounded-xl hover:bg-blue-100">
-                        Produk
-                    </a>
-                </li>
-                <li>
-                    <a href="#" class="block p-3 rounded-xl hover:bg-blue-100">
-                        Transaksi
-                    </a>
-                </li>
-                <li>
-                    <a href="#" class="block p-3 rounded-xl hover:bg-blue-100">
-                        Setting
-                    </a>
-                </li>
-            </ul>
-        </div>
-    </aside>
+<body>
+    <div class="container">
+        <h1>Laravel</h1>
+        <p>Welcome to Laravel</p>
+    </div>
 </body>
-
 </html>
